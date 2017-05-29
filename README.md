850a14c470ded567d4166bb679df154ef83082e9
AntoineMoutou <moutou.antoine@hotmail.fr>
2017-05-29 13:58:47 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # UOM3D
+Visualization of the buildings of the University of Melbourne.
---
+++ js/main.js
@@ -0,0 +1,26 @@
+function setHTML() {
+  var height = document.getElementById("min").offsetHeight +'px';
+  document.getElementById("header").style.top = height;
+}
+
+function set_dev_choice() {
+  document.getElementById("cb1").onclick = function() {
+    if (document.getElementById("cb1").checked){
+      var height = document.getElementById("nav").offsetHeight +'px';
+    }
+    else{
+      var height = document.getElementById("min").offsetHeight +'px';
+    }
+    document.getElementById("header").style.top = height;
+  }
+  }
+
+
+
+function main() {
+
+  setHTML();
+  set_dev_choice();
+}
+
+main();
