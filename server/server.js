14a0c49b05641b06c0251c971b6cd528c615b728
uom3dproject <uom3dproject@gmail.com>
2017-06-01 12:43:35 +1000

    code documentation

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 /**
- * @fileOverview Projet Jeu du taquin
+ * @fileOverview UOM3D Project
  * @author Antoine Moutou <antoinem@student.unimelb.edu.au>
  * @see <a href="https://github.com/antoinemoutou/UOM3D">https://github.com/antoinemoutou/UOM3D</a>
- *@requires http        : pour créer le serveur
- *@requires express     : pour faciliter l'écriture bas niveau de node
- *@requires url         : Avoir le nom de l'url
- *@requires querystring : faire un dictionnaire à partir des arguments passés en GET dans l'url
- *@requires fs          : pouvoir écrire sur le disque un fichier (ici .zip et .shp)
- *@requires unzip2      : permet de déziper le .zip écrit sur le fichier
- *@requires multer      : permet de gérer la réception des formdata
- *@requires JSZip       : permet la compréhension et l'écriture de zip
+ *@requires http        : to create the server
+ *@requires express     : to make easier the low-level language in node
+ *@requires url         : to have the name of the url
+ *@requires querystring : to make a dictionary with the GET arguments
+ *@requires fs          : to write and read files and folders
+ *@requires unzip2      : to unzip the .zip written of the file
+ *@requires multer      : to manage the reception of formdata
+ *@requires JSZip       : to manage the zip
 */
 const http          = require('http');
 const express       = require('express');
@@ -21,79 +21,53 @@ const unzip         = require('unzip2');
 const multer        = require('multer');
 const JSZip         = require("jszip");
 
-/*
-app.set(function(){
-  app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return next();
-  });
-  app.use(express.static(path.join(application_root, "StaticPages")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-*/
-
-
-
-app.get('*',function(req,res){
-	
-	res.set('Access-Control-Allow-Origin','*');
-	res.set('Access-Control-Allow-Headers', 'Origin');
-	
-	/*
-	console.log(req.body);
-	console.log(req.hostname);
-	console.log(req.baseUrl);
-	console.log(req.path);
-	console.log(req.hostname+req.path);
-	*/
-	
-	var path = req.path.substring(1,req.path.length);
-	console.log(path);
-	
-	var content;
-
-	//fs.readFile("C:/Users/Antoine/Desktop/UoM/antoinemoutou/website/UOM3D/server/data/test1_geometry_MasterJSON/test1_geometry_MasterJSON.json",'r', function(err,fd){
-	fs.readFile(path,'utf8',function(err,data){
-		if (err) throw err;
-		
-		else{
-			
-			//console.log(data);
-			res.send(data);
-		}						
-	});
-
-	
-	//content = content.substring(1,content.length-1);
-	//console.log(content);
-	
-	
-	
-	//res.send(content);
-});
-
-
-//app.set("Access-Control-Allow-Origin","*");
-
-
-
 /**
- * Dezip le fichier out.zip contenu dans le dossier zip et écrit son contenu dans le dossier shp
+ * Unzip the file out.zip in the zip folder and write his content in the folder named data
  */
 function dezip(name) {
     fs.createReadStream('zip/' + name + '.zip').pipe(unzip.Extract({ path: 'data/'+name }));
     console.log("Extracted " + 'zip/' + name + '.zip' + " in folder data/"+name+".");
 }
 
-
-
 var upload = multer( { limits:
   {
     fieldNameSize: 999999999,
     fieldSize: 999999999
-  }})
+  }});
+  
+/**
+* Transform a string of the formdata in matrix Uint8array
+* @param str {str} - The string that contains the data of the .zip
+* @return {Uint8Array} - This same string in Uint format
+*/
+function convertBinaryStringToUint8Array(str) {
+  var arr = str.split(",").map(function (val) {
+	return Number(val);
+  })
+  return new Uint8Array(arr)
+}
+
+app.get('*',function(req,res){
+	
+	res.set('Access-Control-Allow-Origin','*');
+	res.set('Access-Control-Allow-Headers', 'Origin');
+	
+	var path = req.path.substring(1,req.path.length);
+	console.log(path);
+	
+	var content;
+
+	fs.readFile(path,'utf8',function(err,data){
+		if (err) throw err;
+		
+		else{
+			res.send(data);
+		}						
+	});
+});
 
 app.post('/',upload.single('zip'), function (req, res, next) {
+  
   var data      = '';
   var name      = req.body.name;
 
@@ -104,12 +78,10 @@ app.post('/',upload.single('zip'), function (req, res, next) {
 /**
 * @function
 * @name save
-* @description Fonction de sauvegarde du .zip sur le serveur, elle fait ensuite appel aux fonctions dezip & python_call
-* @param data {Uint8Array} - Le contenu du .zip transmis par le client
-* @param name {name} - le nom du zip/fichier envoyé par le client
-* @param operation {string} - Le nom de l'opération demandé par le client
+* @description Save function of the .zip on the server
+* @param data {Uint8Array} - The content of the .zip send by the client
+* @param name {name} - the name of zip/file send by the client
 */
-
   function save(data,name) {
       var zip = new JSZip();
       zip.loadAsync(data).then(function () {
@@ -133,22 +105,7 @@ app.post('/',upload.single('zip'), function (req, res, next) {
   else {
     send("error");
   }
-})
+});
 
 app.listen(8080);
 console.log("Serveur ouvert à l'adresse http://127.0.0.1:8080/");
-//dezip()
-
-
-
-/**
-* Transformation de chaine de caractère contenus dans le formdata en matrice Uint8array
-* @param str {str} - La chaine de caractère contenant les données du .zip
-* @return {Uint8Array} - Cette même chaîne de caractère en format Uint
-*/
-function convertBinaryStringToUint8Array(str) {
-  var arr = str.split(",").map(function (val) {
-  return Number(val);
-  })
-  return new Uint8Array(arr)
-}
